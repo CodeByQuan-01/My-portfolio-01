4ae082e3868ae06d46a0c684a097ca2c4d2fbe58
Quan <Kadirisuccess10@gmail.com>
2025-02-04 01:47:37 +0100

    Some fixies

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["placeholder.com"], // Add any domains you'll be loading images from
+    domains: ["placeholder.com", "res.cloudinary.com"], // Add any domains you'll be loading images from
   },
   env: {
     RESEND_API_KEY: process.env.RESEND_API_KEY,
